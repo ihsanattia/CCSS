88769e57b91c655185d477c4a0c047e8e34ee2da
ihsanattia <ihsan.attia@gmail.com>
2019-03-02 00:25:25 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,71 @@
+﻿const	express = require('express'),
+		bodyParser = require("body-parser"),
+		controller = require('./app/controllers/controller'),
+		fs = require('fs'),
+		http = require('http'),
+		https = require('https');		
+	
+const app = express();
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/app', { dotfiles: 'allow' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.render('ccss', {
+		calendarEvents : JSON.stringify(controller.getEvents())
+	});
+});
+
+app.get('/Attendance', function(req, res){
+	res.render('attendance', {
+		date : req.query.date,
+		covers : controller.deriveCovers(req.query.date)
+	});
+});
+
+app.get('/UpdateAvailability', function(req, res){
+	res.render('updateavailability', {
+		date : req.query.date,
+		year : req.query.year,
+		subject : req.query.subject,
+		teachers : controller.getTeachers(),
+		cover : controller.deriveCover(req.query.date, req.query.year, req.query.subject)
+	});
+});
+
+app.post('/server',function(req, res){
+	console.log('here');
+	const data = req.body;
+	if(controller.saveCover(data) == true) {
+		res.json({success : 'Updated Successfully', status : 200});
+	} else {
+		res.json({status : 404});
+	}
+});
+
+app.use(function(req, res, next) {
+  return res.status(404).send('Sorry cant find that page! Or encountered an error! Please raise to management');
+});
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/ccss.cf/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/ccss.cf/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/ccss.cf/chain.pem', 'utf8');
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(process.env.PORT || 3000, () => {
+	console.log('HTTP Server running on port 8080');
+});
+
+httpsServer.listen(8443, () => {
+	console.log('HTTPS Server running on port 8443');
+});
---
+++ app/controllers/controller.js
@@ -0,0 +1,198 @@
+const config = require('./../config/config.json'),
+	  fs = require('fs'),
+	  aws = require('aws-sdk');
+	
+const s3 = new aws.S3({ accessKeyId: config.s3.awsId, secretAccessKey: config.s3.awsSecretKey });
+
+var schoolDates, defaultCovers, savedCovers, teachers;
+
+var getParams = {
+	Bucket: config.s3.bucket,
+	Key: config.s3.env + '/dates.json'
+}	
+s3.getObject(getParams, function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		 schoolDates = JSON.parse(data.Body.toString());
+	}
+})
+
+getParams = {
+	Bucket: config.s3.bucket,
+	Key: config.s3.env + '/defaultCover.json'
+}	
+s3.getObject(getParams, function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		 defaultCovers = JSON.parse(data.Body.toString());
+	}
+})
+
+getParams = {
+	Bucket: config.s3.bucket,
+	Key: config.s3.env + '/covers.json'
+}	
+s3.getObject(getParams, function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		 savedCovers = JSON.parse(data.Body.toString());
+	}
+})	
+
+getParams = {
+	Bucket: config.s3.bucket,
+	Key: config.s3.env + '/teachers.json'
+}	
+s3.getObject(getParams, function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		 teachers = JSON.parse(data.Body.toString());
+	}
+})	
+
+function getEvents() {
+	var events = [];
+	for(var i=0; i<schoolDates.dates.length; i++) {
+		var eventData = 
+		{
+			title: 'View Attendance',
+			url: '/Attendance?date='+schoolDates.dates[i].date,
+			start: formatDate(new Date(schoolDates.dates[i].date))
+		};
+		events.push(eventData);
+	}
+	return events;
+}
+
+function getTeachers() {
+	return teachers.teachers;
+}
+
+function formatDate(date) {
+	var day = ("0" + date.getDate()).slice(-2);
+	var month = ("0" + (date.getMonth() + 1)).slice(-2);
+	var year = date.getFullYear();
+
+	return year + '-' + month + '-' + day;
+}	
+
+function deriveCovers(date) {
+	var covers = []
+	
+	for(var i=0; i<defaultCovers.defaults.length; i++) {
+		
+		var defaultCover = defaultCovers.defaults[i];
+		var cover = savedCovers.covers.find(c => c.date === date && c.year === defaultCover.year && c.subject === defaultCover.subject);
+
+		if(cover) {
+			covers.push(cover);
+		} else {
+			cover = 
+			{
+				date : date,
+				year : defaultCover.year,
+				subject : defaultCover.subject,
+				teachers : [{name : defaultCover.teacher}]
+			}
+			covers.push(cover);
+		}
+		
+	}
+	
+	return covers;
+}
+
+function deriveCover(date, year, subject) {
+	var covers;
+	
+	var cover = savedCovers.covers.find(c => c.date === date && c.year === year && c.subject === subject);
+
+	if(cover) {
+		return cover.teachers;
+	} else {
+		var defaultCover = defaultCovers.defaults.find(d => d.year === year && d.subject === subject);
+		if(defaultCover) {
+			return [{name : defaultCover.teacher}];
+		}
+	}
+	
+	return;
+}
+
+function saveCover(data) {
+	var success = false;
+	try {
+		var date = data.date;
+		var year = data.year;
+		var subject = data.subject;
+		var teachers = data.teachers;
+		
+		var cover = savedCovers.covers.find(c => c.date === date && c.year === year && c.subject === subject);
+		var defaultCover = defaultCovers.defaults.find(d => d.year === year && d.subject === subject);
+		
+		var requireSave = true;
+		
+		if(cover) {
+			if(cover.teachers.length == teachers.length) {
+				var same = true;
+				for(var i=0; i<teachers.length; i++) {
+					if(!cover.teachers.find(t => t.name === teachers[i].name)) {
+						same = false;
+					}
+				}
+				if(same === true) {
+					requireSave = false;
+				}
+			}
+		} else if (defaultCover) {
+			if(teachers && teachers.length == 1 && teachers[0].name === defaultCover.teacher){
+				requireSave = false;
+			}
+		}
+		
+		if(requireSave === true) {
+			if(cover) {
+				var index = savedCovers.covers.indexOf(cover);
+				savedCovers.covers.splice(index, 1);
+			}
+			
+			if(defaultCover && teachers && teachers.length == 1 && teachers[0].name === defaultCover.teacher) {
+			} else {
+				savedCovers.covers.push(data);
+			}
+			
+			console.log("Covers updated for " + date + " year " + year + " " + subject);
+			console.log(data);
+
+			var putParams = {
+				Body: JSON.stringify(savedCovers),
+				ServerSideEncryption: 'AES256',
+				Bucket: bucket,
+				Key: env + '/covers.json'
+			};
+			s3.putObject(putParams, function(err, data) {
+				if (err) {
+					console.log(err, err.stack);
+				}
+			});
+		}
+		success = true;
+	}
+	catch (error) {
+		console.log(error);
+		success = false;
+	}
+	finally {
+		return success;
+	}
+}
+
+exports.getEvents = getEvents;
+exports.deriveCovers = deriveCovers;
+exports.deriveCover = deriveCover;
+exports.saveCover = saveCover;
+exports.getTeachers = getTeachers;
